1e920068267c00fbfe776d7c04e1af2bac7afa9b
StevenHD <hlhdsteven825@outlook.com>
2020-11-18 16:19:01 +0800

    add epoll

--- main.cpp
+++ main.cpp
@@ -16,26 +16,34 @@
 #include <streambuf>
 #include <vector>
 #include <iterator>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/epoll.h>
 
-int createListenFD(int &port)
+int createListenFD(const char* IP, int &port)
 {
+    /* 创建套接字 */
     int listen_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 
     // set socket options for "reuse address"
-    int on = 1;
-    setsockopt(listen_socket_fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+    int on = 1, res = -1;
+    res = setsockopt(listen_socket_fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+    if (res == -1) std::cout << "Failed to set socket options!" << std::endl;
 
+    /* 初始化服务器 */
     struct sockaddr_in server_addr;
-    bzero(&server_addr, sizeof(server_addr));
-
-    /* 填充地址结构体 */
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);   // 小端转大端
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     socklen_t server_len = sizeof(server_addr);
 
+    /* 填充地址结构体 */
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;               // 地址族
+    inet_pton(AF_INET, IP, &server_addr.sin_addr);
+    server_addr.sin_port = htons(port);   // 小端转大端, 设置端口
+    // server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // 监听本机所有的IP
+
+
     // bind IP and port
-    int res = bind(listen_socket_fd, (struct sockaddr*)&server_addr, server_len);
+    res = bind(listen_socket_fd, (struct sockaddr*)&server_addr, server_len);
     if (res == -1) perror("bind");
 
     /* start listen.. */
@@ -99,45 +107,99 @@ void http_parser(int& client_fd, char * msg)
 
 int main(int argc, char * argv[])
 {
-    if (argc < 2)
+    if (argc <= 2)
     {
         printf("eg: WebServer port\n");
         exit(-1);
     }
-    int port = atoi(argv[1]);
+
+    const char* IP = argv[1];
+    int port = atoi(argv[2]);   // string to int
 
     /* 创建监听套接字，返回一个套接字的文件描述符 */
-    int listen_socket_fd = createListenFD(port);
+    int listen_socket_fd = createListenFD(IP, port);
 
     /* 初始化客户端的地址 */
     struct sockaddr_in client_addr;
     socklen_t client_len = sizeof(client_addr);
 
+    /* 创建epoll树的根节点 */
+    int epoll_fd = epoll_create(2000);
+
+    /* epoll树是用来检测节点对应的文件描述符有没有发生变化 */
+    /* 初始化epoll树 */
+    struct epoll_event ev; // 往树上挂节点本质上是挂一个结构体, 所以初始化一个结构体; ev对应的是listen_socket_fd中的信息
+    ev.events = EPOLLIN;
+    ev.data.fd = listen_socket_fd;
+
+    /* epoll_wait()中的第2个参数，用来通知是哪些事件发生了变化 */
+    struct epoll_event events[2000];
+
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listen_socket_fd, &ev);
     while (1) {
+        /* 使用epoll通知内核进行文件流的检测 */
+        int ret = epoll_wait(epoll_fd, events, sizeof(events) / sizeof(events[0]), -1);
 
-        /* 接收客户端的连接 */
-        int connection_fd = accept(listen_socket_fd, (struct sockaddr*)&client_addr, &client_len); // 这里会阻塞，有结果才会往下走
-        if (connection_fd == -1)
+        /* 通过epoll_wait()可以知道这棵树上有ret个节点发生了变化 */
+        /* 遍历events数组中的前ret个元素 */
+        for (int i = 0; i < ret; i ++ )
         {
-            perror("accept error");
-            exit(-1);
-        }
-        printf("有客户端连接\n");
+            int cur_fd = events[i].data.fd;
 
-        /* 初始化缓冲区，用来接收客户端发送的数据 */
-        /* 处理客户端发来的请求 */
-        char read_buf[1024] = {0};
-        int recv_len = recv(connection_fd, read_buf, sizeof(read_buf), 0);
-        if (recv_len == -1) {
-            perror("recv error");
-            exit(-1);
-        } else if (recv_len == 0) {
-            printf("client disconnected..");
-            close(connection_fd);
-        } else {
-            printf("The client's request is: %s\n", read_buf);
-            http_parser(connection_fd, read_buf);
-            close(connection_fd);
+            /* 判断是否有新连接 */
+            if (cur_fd == listen_socket_fd)
+            {
+                /* 接收客户端的连接请求 */
+                int connection_fd = accept(listen_socket_fd, (struct sockaddr*)&client_addr, &client_len); // 这里会阻塞，有结果才会往下走
+                if (connection_fd == -1)
+                {
+                    perror("accept error");
+                    exit(-1);
+                }
+                printf("有客户端连接\n");
+
+                /* 将新得到的connection_fd挂到epoll树上 */
+                struct epoll_event tmp;
+                tmp.events = EPOLLIN;
+                tmp.data.fd = connection_fd;
+                epoll_ctl(epoll_fd, EPOLL_CTL_ADD, connection_fd, &tmp); // 将connection_fd的信息存入到tmp这个结构体变量中
+
+                /* 打印客户端信息 */
+                char client_ip[64] = {0};
+                printf("New Client IP: %s, Port: %d\n",
+                        inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, client_ip, sizeof(client_ip)),
+                        ntohs(client_addr.sin_port));
+            }
+            else
+            {
+                /* 处理已经连接的客户端发送过来的数据 */
+                /* 这里只处理"读操作" */
+                if ((events[i].events & EPOLLIN) == 0) continue;
+
+                /* 读数据 */
+                /* 初始化缓冲区，用来接收客户端发送的数据 */
+                /* 处理客户端发来的请求 */
+                char read_buf[1024] = {0};
+                int readData_len = recv(cur_fd, read_buf, sizeof(read_buf), 0);
+                if (readData_len == -1) {
+                    perror("recv error");
+                    exit(-1);
+                }
+                else if (readData_len == 0) {
+                    printf("client disconnected..");
+
+                    /* 将fd从epoll树上删掉 */
+                    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, cur_fd, NULL);
+
+                    close(cur_fd);
+                }
+                else {
+                    printf("The client's request is: %s\n", read_buf);
+                    http_parser(cur_fd, read_buf);
+                    // write(cur_fd, read_buf, readDate_len);
+                    close(cur_fd);
+                }
+            }
         }
     }
     close(listen_socket_fd);
